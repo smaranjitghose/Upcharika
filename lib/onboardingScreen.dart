64837d9c37b999c4608d804119f5db6cd5866c83
Priyanshu Paliwal <66347715+Priyanshu078@users.noreply.github.com>
2021-06-29 10:49:02 +0530

    Updated onpressed button of OnboardingScreen

--- lib/onboardingScreen.dart
+++ lib/onboardingScreen.dart
@@ -75,9 +75,7 @@ class OnboardingScreen extends StatelessWidget {
                   onPressed: () {
                     Navigator.pushAndRemoveUntil(
                         context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                MyHomePage(title: 'Upcharika')),
+                        MaterialPageRoute(builder: (context) => BottomNavbar()),
                         (route) => false);
                   },
                   child: Text(
