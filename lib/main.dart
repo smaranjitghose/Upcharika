5bf85c1b0fe7333280c26a9b60c4d81b5cb4aa96
aviralguptaa <aviralrakeshgupta@gmail.com>
2021-07-04 14:05:13 +0530

    Imported the packages and classes also modified the pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:upcharika/HeartRate.dart';
 import 'package:upcharika/Home.dart';
 import 'package:upcharika/homePage.dart';
 import 'package:upcharika/Level.dart';
+import 'onboardingScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 int firstRun; // variable which will decide where our app will go
 Future<void> main() async {
